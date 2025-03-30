53990dc5bde8a16b5e579f33a9f776129ff18fd8
kunleoduntan <90121650+kunleoduntan@users.noreply.github.com>
2025-03-30 14:10:58 +0100

    v1

---
+++ estate/estate_management/doctype/property_fee_type/test_property_fee_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Kunle Oduntan	 and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPropertyFeeType(FrappeTestCase):
+	pass
